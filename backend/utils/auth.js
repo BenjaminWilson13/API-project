1ae314ac28de11f772d6e3d0f5d14c76093d7a54
Benjamin Wilson <benjamin.blake.wilson13@gmail.com>
2023-04-19 12:23:01 -0500

    finsied add image to Event route

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -64,9 +64,9 @@ const restoreUser = (req, res, next) => {
 const requireAuth = function (req, _res, next) {
     if (req.user) return next();
 
-    const err = new Error('Authentication required');
-    err.status = 401;
-    return next(err);
+    return _res.json({
+        message: "Authentication required"
+    })
 }
 
 const credentialCatcher = ((req, res, next) => {

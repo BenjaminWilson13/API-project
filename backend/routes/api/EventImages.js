81713a40fe61608959edfdcb0b937c80531192ad
Benjamin Wilson <benjamin.blake.wilson13@gmail.com>
2023-04-20 12:28:43 -0500

    fixed routes up and moved add image to event to the events router file

--- backend/routes/api/EventImages.js
+++ backend/routes/api/EventImages.js
@@ -11,72 +11,12 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
-router.post('/:eventId/images', requireAuth, async (req, res, next) => {
-    console.log('lul wut')
 
-    const eventFind = await Event.findByPk(req.params.eventId); 
-    if (!eventFind) {
-        res.status(404); 
-        return res.json({ 
-            message: "Event couldn't be found"
-        }); 
-    }; 
 
-    const eventAttending = await Event.findByPk(req.params.eventId, {
-        include: [{
-            model: User, 
-            through: {
-                model: Attendance, 
-                where: {
-                    status: 'attending'
-                }, 
-                attributes: ['status']
-            }, 
-            where: {
-                id: req.user.id
-            }, 
-            attributes: ['id']
-        }], 
-        attributes: ['id']
-    }); 
-
-    
-    const eventOrganizerCohost = await Event.findByPk(req.params.eventId, {
-        include: {
-            model: Group, 
-            include: {
-                model: Membership, 
-                where: {
-                    status: {
-                        [Op.or]: ['organizer', 'co-host']
-                    }, 
-                    userId: req.user.id
-                }, 
-                attributes: ['status']
-            }, 
-            attributes: ['id']
-        }
-    })
-    
-    if (!eventAttending && eventOrganizerCohost.dataValues.Group === null) {
-        res.status(403); 
-        return res.json({ 
-            message: "Forbidden"
-        }); 
-    }; 
-    
-
-    
-    const eventId = req.params.eventId; 
-    const {url, preview} = req.body; 
-    const image = await EventImage.create({eventId, url, preview})
-    const obj = {
-        id: image.id, 
-        url: image.url, 
-        preview: image.preview
-    }
-    return res.json(obj);
-}); 
-
+//Delete and Image for an Event
+router.delete('/:imageId', requireAuth, async (req, res, next) => {
+    console.log('lol'); 
+    res.json(req.user)
+})
 
 module.exports = router; 
--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -3,11 +3,12 @@ const bcrypt = require('bcryptjs');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { Group, Membership, GroupImage, Venue, User, Event, Attendance, EventImage } = require('../../db/models');
+const { Sequelize, Op, Model, DataTypes } = require("sequelize");
 
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
-const eventImageRouter = require('./EventImages.js'); 
+ 
 const attendeeRouter = require('./attendees.js')
 
 const router = express.Router();
@@ -62,8 +63,7 @@ router.get('/', async (req, res, next) => {
     res.json(obj);
 })
 
-//Including the image router for /api/events/:eventId/images
-router.use(eventImageRouter); 
+
 
 //Including the attendees router for /api/events/:eventId/attendees
 router.use(attendeeRouter); 
@@ -228,6 +228,74 @@ router.post('/:groupId/events', requireAuth, async (req, res, next) => {
     const event = await Event.create({ venueId, groupId, name, description, type, capacity, price: parseFloat(price.toFixed(2)), startDate, endDate });
     res.json(event);
 })
+
+//Add an image to an event based on the Event's Id
+router.post('/:eventId/images', requireAuth, async (req, res, next) => {
+    console.log('lul wut')
+
+    const eventFind = await Event.findByPk(req.params.eventId); 
+    if (!eventFind) {
+        res.status(404); 
+        return res.json({ 
+            message: "Event couldn't be found"
+        }); 
+    }; 
+
+    const eventAttending = await Event.findByPk(req.params.eventId, {
+        include: [{
+            model: User, 
+            through: {
+                model: Attendance, 
+                where: {
+                    status: 'attending'
+                }, 
+                attributes: ['status']
+            }, 
+            where: {
+                id: req.user.id
+            }, 
+            attributes: ['id']
+        }], 
+        attributes: ['id']
+    }); 
+
+    
+    const eventOrganizerCohost = await Event.findByPk(req.params.eventId, {
+        include: {
+            model: Group, 
+            include: {
+                model: Membership, 
+                where: {
+                    status: {
+                        [Op.or]: ['organizer', 'co-host']
+                    }, 
+                    userId: req.user.id
+                }, 
+                attributes: ['status']
+            }, 
+            attributes: ['id']
+        }
+    })
+    
+    if (!eventAttending && eventOrganizerCohost.dataValues.Group === null) {
+        res.status(403); 
+        return res.json({ 
+            message: "Forbidden"
+        }); 
+    }; 
+    
+
+    
+    const eventId = req.params.eventId; 
+    const {url, preview} = req.body; 
+    const image = await EventImage.create({eventId, url, preview})
+    const obj = {
+        id: image.id, 
+        url: image.url, 
+        preview: image.preview
+    }
+    return res.json(obj);
+}); 
 
 
 //Edit an Event specified by id
